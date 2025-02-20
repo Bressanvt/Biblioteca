4dda1f12d80d9a16a67ce8ca9fc357ba534f6956
Bressanvt <vitorbressanvieira@outlook.com>
2025-02-20 16:14:22 -0300

    Adição da DAO e paremetros do Controller

--- App/Controller/AlunoController.php
+++ App/Controller/AlunoController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Model\Aluno;
+
 class AlunoController
 {
   public static function cadastro()
   {
-    echo "vou mostrar o formulario a depender";
+    echo "vou mostrar o formulario a depender...";
+
+    $model = new Aluno();
+    $model->id = 8;
+    $model->nome = "Vitor";
+    $model->ra = 123;
+    $model->curso = "Desenvolvimento de Sistemas";
+    $model->save();
   } 
 
 
   public static function listar()
   {
     echo "listagem de alunos";
+    $aluno = new Aluno();
+    $aluno->getAllRows();
   }
 
 }
--- App/DAO/AlunoDAO.php
+++ App/DAO/AlunoDAO.php
@@ -1,6 +1,39 @@
 <?php
 
+namespace App\Dao;
+
+use App\Model\Aluno;
 class AlunoDAO
 {
-    
+    public function save(Aluno $model) : Aluno
+    {
+        return ($model ->id == null) ? $this->insert($model) : $this->update($model);
+    }
+
+    public function insert(Aluno $Model): Aluno
+    {
+        return new Aluno();
+    }
+
+    public function update(): Aluno
+    {
+        return new Aluno(); 
+    }
+
+    public function selectById(int $id): ?Aluno
+    {
+        return new Aluno();  
+    }
+
+    public function select(): array
+    {
+        return []; 
+    }
+
+
+    public function delete(int $id) : bool
+    {
+        return false ;  
+    }
+
 }
